4c88f0740383b50315ec6ba125d93e4062b1ebcd
mwendarapho <mwendaraphael@gmail.com>
2022-01-08 15:40:04 +0300

    print header

---
+++ resources/views/layouts/print_page_header.blade.php
@@ -0,0 +1,5 @@
+ <div class="col-md-12 text-center d-none d-print-block">
+        <h2>{{ env('ORG_NAME')}} </h2>
+        <h3>{{ env('ORG_ADDRESS')}}, {{ env('ORG_CITY')}} </h3>
+        <p>{{ env('ORG_EMAIL')}} | {{ env('ORG_PHONE')}} </p>
+    </div>
